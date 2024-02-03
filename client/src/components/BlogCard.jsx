d16c53bbea4f60b43c5c276b9b10cbbca9bcac3b
Shyaminda <shyamindaSenevirathna@gmail.com>
2024-02-03 13:09:01 +0530

    add blog page

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -3,17 +3,15 @@ import { Link } from 'react-router-dom';
 
 const BlogCard = () => {
     return (
-        <div className="col-3">
-            <div className="blog-card">
-                <div className="card-image">
-                    <img src=" images/blog-1.jpg" alt="blog" className='img-fluid' />
-                </div>
-                <div className="blog-content">
-                    <p className="date">29 jan, 2024</p>
-                        <h5 className="title">beautiful sunday morning</h5>
-                        <p className="description">lorem Voluptatem itaque in quasi. Soluta perspiciatis et inventore sit dolor odio. Sequi reiciendis id dicta qui at et. Animi ad sapiente officia nesciunt est repudiandae ut.</p>
-                        <Link to="/" className='button'>READ MORE</Link>   
-                </div>
+        <div className="blog-card">
+            <div className="card-image">
+                <img src=" images/blog-1.jpg" alt="blog" className='img-fluid w-100' />
+            </div>
+            <div className="blog-content">
+                <p className="date">29 jan, 2024</p>
+                    <h5 className="title">beautiful sunday morning</h5>
+                    <p className="description">lorem Voluptatem itaque in quasi. Soluta perspiciatis et inventore sit dolor odio. Sequi reiciendis id dicta qui at et. Animi ad sapiente officia nesciunt est repudiandae ut.</p>
+                    <Link to="/" className='button'>READ MORE</Link>   
             </div>
         </div>
     )
