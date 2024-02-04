80c8ee89c65978cd9527c7bed13d1175e165adcf
Shyaminda <shyamindaSenevirathna@gmail.com>
2024-02-05 04:13:29 +0530

    Refactor password input fields in customInput component

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Meta from '../components/Meta';
 import BreadCrumbs from '../components/BreadCrumbs';
 import Container from '../components/Container';
+import CustomInput from '../components/CustomInput';
 
 const ResetPassword = () => {
     return (
@@ -14,12 +15,8 @@ const ResetPassword = () => {
                     <div className="auth-card">
                         <h3 className='text-center mb-3'>Reset Password</h3>
                         <form className='d-flex flex-column gap-20'>
-                            <div>
-                                <input type="password" name='password' placeholder="Password" className='form-control' />
-                            </div>
-                            <div>
-                                <input type="password" name='confirm password' placeholder="Confirm Password" className='form-control' />
-                            </div>
+                            <CustomInput type="password" name='password' placeholder="Password" className='form-control' />
+                            <CustomInput type="password" name='confirm password' placeholder="Confirm Password" className='form-control' />
                             <div>
                                 <div className='d-flex justify-content-center mt-3 gap-15 align-items-center'>
                                     <button type='submit' className='button signIn'>Change Password</button>
