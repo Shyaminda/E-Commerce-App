49c9633004e8e21febf94e586437b398e1c2fd03
Shyaminda <shyamindaSenevirathna@gmail.com>
2024-02-05 04:10:12 +0530

    Add CustomInput component to ForgotPassword page

--- client/src/pages/ForgotPassword.jsx
+++ client/src/pages/ForgotPassword.jsx
@@ -3,6 +3,7 @@ import Meta from '../components/Meta';
 import BreadCrumbs from '../components/BreadCrumbs';
 import { Link } from 'react-router-dom';
 import Container from '../components/Container';
+import CustomInput from '../components/CustomInput';
 
 const ForgotPassword = () => {
     return (
@@ -16,11 +17,8 @@ const ForgotPassword = () => {
                         <h3 className='text-center mb-3'>Reset Your Password</h3>
                         <p className="text-center mt-2 mb-3">We Will Send You a Email To Reset Your Password</p>
                         <form className='d-flex flex-column gap-20'>
+                            <CustomInput type="email" name='email' placeholder="Email" className='form-control' />
                             <div>
-                                <input type="email" name='email' placeholder="Email" className='form-control' />
-                            </div>
-                            <div>
-                                
                                 <div className='d-flex justify-content-center flex-column mt-3 gap-15 align-items-center'>
                                     <button className='button signIn' type='submit'>Submit</button>
                                     <Link to="/signIn">Cancel</Link>
