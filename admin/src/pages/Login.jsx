0f92c6b9d9a9a0fc548d7802cbab8aba7213f4bd
Shyaminda <shyamindaSenevirathna@gmail.com>
2024-02-15 23:27:26 +0530

    Add Bootstrap CSS and JS files, and update login,reset password and forgot password pages

--- admin/src/pages/Login.jsx
+++ admin/src/pages/Login.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import CustomInput from '../components/CustomInput';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-    <div>Login</div>
-    )
+    <div className='py-5' style={{"background":"#ffd333","minHeight":"100vh"}}>
+        <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
+            <h3 className='text-center'>Login</h3>
+            <p className='text-center'>Login to your account to continue.</p>
+                <form>
+                    <CustomInput type="text" label="email" id="email" />
+                    <CustomInput type="password" label="password" id="pass" />
+                    <div className='mb-3 text-end'>
+                        <Link to="forgot-password" className=''>Forgot Password?</Link>
+                    </div>
+                    <Link to="/admin" className='border-0 px-3 py-2 text-white fw-bold w-100 text-center text-decoration-none' type='submit' style={{"background":"#ffd333"}}>Login</Link>
+                </form>
+        </div>
+    </div>
+    );
 }
 
 export default Login;
--- admin/src/pages/ResetPassword.jsx
+++ admin/src/pages/ResetPassword.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import CustomInput from '../components/CustomInput';
 
 const ResetPassword = () => {
     return (
-    <div>ResetPassword</div>
-    )
+    <div className='py-5' style={{"background":"#ffd333","minHeight":"100vh"}}>
+        <div className="my-5 w-25 bg-white rounded-3 mx-auto p-4">
+            <h5 className='text-center'>Reset Password</h5>
+            <p className='text-center small'>Please confirm your password.</p>
+                <form>
+                    <CustomInput type="password" label="new password" id="pass" />
+                    <CustomInput type="password" label="confirm password" id="confirmPass" />
+                    <button className='border-0 px-3 py-2 text-white fw-bold w-100' type='submit' style={{"background":"#ffd333"}}>Reset Password</button>
+                </form>
+        </div>
+    </div>
+    );
 }
 
 export default ResetPassword;
