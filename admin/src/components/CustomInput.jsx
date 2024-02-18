90a9a332fd96291bbe42a877d5ff304654118493
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2024-02-18 17:27:11 +0530

    second commit after the crash

--- admin/src/components/CustomInput.jsx
+++ admin/src/components/CustomInput.jsx
@@ -2,11 +2,7 @@ import React from 'react';
 import { Input } from 'antd';
 
 const CustomInput = (props) => {
-<<<<<<< HEAD
     const { type,label,i_id,i_class,name,value,onChange,onBlur } = props;
-=======
-    const { type,label,i_id,i_class } = props;
->>>>>>> ad988b7bcb74db410a0ca1d147bb3a10dfe4d16c
     return (
         <div className="form-floating mb-3">
             <Input 
@@ -14,13 +10,10 @@ const CustomInput = (props) => {
                 className={`form-control ${i_class}`}
                 id={i_id}
                 placeholder={label}
-<<<<<<< HEAD
                 name={name}
                 value={value}
                 onChange={onChange}
                 onBlur={onBlur}
-=======
->>>>>>> ad988b7bcb74db410a0ca1d147bb3a10dfe4d16c
             />
             <label htmlFor={label}>{label}</label>
         </div>
