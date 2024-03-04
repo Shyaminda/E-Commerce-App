309f5cb0172a858c7fa6f604d81de2132fe0ce9a
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2024-03-05 02:27:18 +0530

    add dynamic functions to featured section in home

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -184,10 +184,57 @@ function Home() {
                     <div className="col-12">
                         <h3 className="section-heading">Feature Collections</h3>
                     </div>
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
+                    {
+                        productState && productState?.map((item,index) => {
+                            if(item.tags === "featured"){
+                                return(
+                                    <div key={index} className={"col-3"}>
+                                    <Link className="product-card position-relative">   {/* to="product/:id" */}
+                                        <div className="wishlist-icon position-absolute">
+                                            <button className='border-0 bg-transparent' onClick={(e)=>{addToWish(item?._id)}}>
+                                                <img src="images/wish.svg" alt="wishlist" />
+                                            </button>
+                                        </div>
+
+                                        <div className="product-image">
+                                            <img src={item.images.length > 0 && item.images[0].url} alt="product" className='img-fluid' />
+                                            <img src="images/watch-1.webp" alt="product" className='img-fluid' />
+                                        </div>
+
+                                        <div className="product-details">
+                                            <h6 className="brand">{item?.brand}</h6>
+                                            <h5 className="product-title">{item?.title}</h5>
+                                            <ReactStars
+                                                count={5}
+                                                size={10}
+                                                value={item?.totalRatings.toString()}
+                                                edit={false}
+                                                activeColor="#ffd700" 
+                                            />
+                                            <p className='price'>$ {item?.price}</p>
+                                        </div>
+                                        
+                                        <div className="action-bar position-absolute">
+                                            <div className="d-flex flex-column gap-10">
+                                                <button className='border-0 bg-transparent'>
+                                                    <img src="images/add-cart.svg" alt="add-cart" />
+                                                </button>
+                                                <button className='border-0 bg-transparent'>
+                                                    <img src="images/prodcompare.svg" alt="prodcompare" />
+                                                </button>
+                                                <button className='border-0 bg-transparent'>
+                                                    <img src="images/view.svg" alt="view" />
+                                                </button>
+                                                
+                                            </div>
+                                        </div>
+                                    </Link>
+                                </div>
+                                )
+                            }
+                            return null;
+                        })
+                    }
                 </div>
             </Container>
 
