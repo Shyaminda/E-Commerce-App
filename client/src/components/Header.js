b461a76647753f7ad6a83db28819ed5fa3dae1fa
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2024-03-14 00:03:09 +0530

    add logout functionality

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -21,6 +21,11 @@ const Header = () => {
     }
   }, [cartState]);
 
+  const handleLogout = () => {
+    localStorage.clear();
+    window.location.reload();
+  };
+
   return (
     <div>
       <header className="header-top-stripe pt-2">
@@ -135,6 +140,7 @@ const Header = () => {
                     <NavLink to="/my-orders">My Orders</NavLink>
                     <NavLink to="/blogs">Blogs</NavLink>
                     <NavLink to="/contact">Contact</NavLink>
+                    <button onClick={handleLogout} className="border border-0 bg-transparent text-white text-uppercase" type="button"> logout</button>
                   </div>
                 </div>
               </div>
